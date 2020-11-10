f16def419b8819e91cd55d0718d4a2ab9b707ae1
sbenny123 <susmi.benny@students.plymouth.ac.uk>
2020-11-10 15:10:18 +0000

    Update MainActivity
    Ensure ARCore is downloaded if not present

--- main/app/src/main/java/com/example/interiordesign/main/MainActivity.java
+++ main/app/src/main/java/com/example/interiordesign/main/MainActivity.java
@@ -3,10 +3,22 @@ package com.example.interiordesign.main;
 import android.widget.Toast;
 
 import com.example.interiordesign.helpers.CameraPermissionHelper;
+import com.google.ar.core.ArCoreApk;
+import com.google.ar.core.Session;
+import com.google.ar.core.exceptions.UnavailableApkTooOldException;
+import com.google.ar.core.exceptions.UnavailableArcoreNotInstalledException;
+import com.google.ar.core.exceptions.UnavailableDeviceNotCompatibleException;
+import com.google.ar.core.exceptions.UnavailableSdkTooOldException;
+import com.google.ar.core.exceptions.UnavailableUserDeclinedInstallationException;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    // Set to true ensures requestInstall() triggers installation if necessary.
+    private boolean mUserRequestedInstall = true;
+
 
     @Override
     protected void onResume() {
@@ -17,6 +29,48 @@ public class MainActivity extends AppCompatActivity {
             CameraPermissionHelper.requestPermission(this);
             return;
         }
+
+        // Ensure Google Play Services for AR and ARCore device profile data are
+        // installed and up to date.
+        try {
+            Session mSession;
+
+            if (mSession == null) {
+                switch (ArCoreApk.getInstance().requestInstall(this, mUserRequestedInstall)) {
+                    case INSTALLED:
+                        // Success, safe to create the AR session.
+                        mSession = new Session(this);
+                        break;
+                    case INSTALL_REQUESTED:
+                        // When this method returns `INSTALL_REQUESTED`:
+                        // 1. This activity will be paused.
+                        // 2. The user is prompted to install or update Google Play
+                        //    Services for AR (market://details?id=com.google.ar.core).
+                        // 3. ARCore downloads the latest device profile data.
+                        // 4. This activity is resumed. The next invocation of
+                        //    requestInstall() will either return `INSTALLED` or throw an
+                        //    exception if the installation or update did not succeed.
+                        mUserRequestedInstall = false;
+                        return;
+                }
+            }
+        } catch (UnavailableUserDeclinedInstallationException e) {
+            // Display an appropriate message to the user and return gracefully.
+            Toast.makeText(this, "TODO: handle exception " + e, Toast.LENGTH_LONG)
+                    .show();
+            return;
+        } catch (UnavailableApkTooOldException e) {
+            e.printStackTrace();
+        } catch (UnavailableDeviceNotCompatibleException e) {
+            e.printStackTrace();
+        } catch (UnavailableUserDeclinedInstallationException e) {
+            e.printStackTrace();
+        } catch (UnavailableArcoreNotInstalledException e) {
+            e.printStackTrace();
+        } catch (UnavailableSdkTooOldException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Override
