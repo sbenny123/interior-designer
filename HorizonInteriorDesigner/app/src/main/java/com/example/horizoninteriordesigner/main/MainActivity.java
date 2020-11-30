c27134b6350872a0c7398efa45d2d5696860bbef
sbenny123 <susmi.benny@students.plymouth.ac.uk>
2020-11-30 11:38:55 +0000

    Update MainActivity.java
    - Add virocore listener

--- HorizonInteriorDesigner/app/src/main/java/com/example/horizoninteriordesigner/main/MainActivity.java
+++ HorizonInteriorDesigner/app/src/main/java/com/example/horizoninteriordesigner/main/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.horizoninteriordesigner.main;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 
+import com.viro.core.ARAnchor;
+import com.viro.core.ARNode;
 import com.viro.core.ARScene;
+import com.viro.core.AmbientLight;
+import com.viro.core.Vector;
 import com.viro.core.ViroView;
 import com.viro.core.ViroViewARCore;
 
@@ -25,6 +30,14 @@ public class MainActivity extends Activity {
             @Override
             public void onSuccess() {
                 arScene = new ARScene();
+
+                SampleARSceneListener arSceneListener = new SampleARSceneListener(new Runnable() {
+                    @Override
+                    public void run() {
+                    }
+                });
+                arScene.setListener(arSceneListener);
+                viroView.setScene(arScene);
             }
 
             @Override
@@ -32,6 +45,53 @@ public class MainActivity extends Activity {
 
             }
         });
+    }
+
+    // You can use the ARSceneListener to respond to AR events, including the detection of
+    // anchors
+    private class SampleARSceneListener implements ARScene.Listener {
+        private Runnable mOnTrackingInitializedRunnable;
+        private boolean mInitialized;
+        public SampleARSceneListener(Runnable onTrackingInitializedRunnable) {
+            mOnTrackingInitializedRunnable = onTrackingInitializedRunnable;
+            mInitialized = false;
+        }
+
+        @Override
+        public void onTrackingUpdated(ARScene.TrackingState trackingState,
+                                      ARScene.TrackingStateReason trackingStateReason) {
+            if (trackingState == ARScene.TrackingState.NORMAL && !mInitialized) {
+                mInitialized = true;
+                if (mOnTrackingInitializedRunnable != null) {
+                    mOnTrackingInitializedRunnable.run();
+                }
+            }
+        }
+
+        @Override
+        public void onTrackingInitialized() {
+            // this method is deprecated.
+        }
+
+        @Override
+        public void onAmbientLightUpdate(float lightIntensity, Vector color) {
+            // no-op
+        }
+
+        @Override
+        public void onAnchorFound(ARAnchor anchor, ARNode arNode) {
+            // no-op
+        }
+
+        @Override
+        public void onAnchorUpdated(ARAnchor anchor, ARNode arNode) {
+            // no-op
+        }
+
+        @Override
+        public void onAnchorRemoved(ARAnchor anchor, ARNode arNode) {
+            // no-op
+        }
     }
 
     @Override
