b637f73a96bedaa3262d95be3da8838abe529e36
sbenny123 <susmi.benny@students.plymouth.ac.uk>
2021-05-01 13:25:20 +0100

    Create initial item viewmodel

---
+++ app/src/main/java/com/example/horizoninteriordesigner/activities/Main/viewModels/ItemViewModel.java
@@ -0,0 +1,18 @@
+package com.example.horizoninteriordesigner.activities.Main.viewModels;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.horizoninteriordesigner.models.Item;
+
+public class ItemViewModel extends ViewModel {
+    private MutableLiveData<Item> item;
+
+    public MutableLiveData<Item> getItem() {
+        return item;
+    }
+
+    public void setItem(MutableLiveData<Item> item) {
+        this.item = item;
+    }
+}
